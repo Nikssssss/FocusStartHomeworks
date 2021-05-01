9e03cf295ec86e4ec59ba8f2433713a1415be650
nikitagusev <nikitagyssev@gmail.com>
2021-05-01 23:23:53 +0700

    Сделал ViewController с информацией о себе

---
+++ Autobiography/DeveloperViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Autobiography
+//
+//  Created by Никита Гусев on 01.05.2021.
+//
+
+import UIKit
+
+class DeveloperViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupView
+    }
+}
+
+private extension DeveloperViewController {
+    func setupView() {
+        
+    }
+    
+    func setupProgrammingInfo() {
+        
+    }
+}
+
